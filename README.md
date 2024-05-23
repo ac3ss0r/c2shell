accb7aa57a605ebd4839dbfc4cb818f9e4b36277
acessor <98286101+ac3ss0r@users.noreply.github.com>
2024-05-23 17:16:37 +0300

    Added a basic overview to readme.md

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ShellcodeLab
-A C/C++ project designed to simplify shellcode creation on any compilers using pure C
+<div align=center style="background-color: transparent;">
+    <img width="100%" src="Images/preview.png"></img>
+</div>
+A C/C++ project designed to simplify shellcode creation on any compilers using C. Supports x86_64, x86_32 and practically any architecture and OS.
+
+## ℹ️ Overview & theory
+A shellcode is an offset-independent assembly code which can be executed from any part of program. Those are commonly used by cyber-security engineers, hackers and lowlevel developers (anticheats, protections, etc). This project presents a way to create shellcodes easily in pure C, without any ASM usage, allowing to write universal shellcodes across architectures/platforms.
+
--- Shellcode/shcutils.h
+++ Shellcode/shcutils.h
@@ -2,9 +2,9 @@
 #define PEB_H
 
 #if defined(_WIN64) || defined(WIN64) || defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
-#define _WINDOWS
+    #define _WINDOWS
 #elif defined(__linux__) || defined(__ANDROID__)
-#define _LINUX
+    #define _LINUX
 #endif
 
 // Create custom sections on both clang & msc++
@@ -22,16 +22,6 @@
     #define INLINE __attribute__((always_inline)) inline // GCC/G++/CLANG
 #endif
 
-#ifdef _MSC_VER
-    #ifdef defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
-        #define NAKED __declspec (naked)
-    #else // no naked on x64 w0mp w0mp
-        #define NAKED
-    #endif
-#else
-    #define NAKED __attribute__((naked))
-#endif
-
 // Prevents functions from inlining forcefully
 #if defined(_MSC_VER)
     #define NOINLINE __declspec(noinline)
@@ -39,70 +29,17 @@
     #define NOINLINE __attribute__((noinline))
 #endif
 
-// On windows we use PEB & TEB, on linux we'll use inline syscalls for shellcoding
-#ifdef _WINDOWS 
-
-#include <windows.h>
-
-#ifndef __NTDLL_H__
-
-#ifndef TO_LOWERCASE
-#define TO_LOWERCASE(out, c1) (out = (c1 <= 'Z' && c1 >= 'A') ? c1 = (c1 - 'A') + 'a': c1)
+#ifdef _MSC_VER
+    #ifdef defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
+        #define NAKED __declspec (naked)
+    #else // no naked on x64 w0mp w0mp (note that's the reason why it will crash)
+        #define NAKED
+    #endif
+#else
+    #define NAKED __attribute__((naked))
 #endif
 
-typedef struct _UNICODE_STRING {
-    USHORT Length;
-    USHORT MaximumLength;
-    PWSTR  Buffer;
-
-} UNICODE_STRING, * PUNICODE_STRING;
-
-typedef struct _PEB_LDR_DATA {
-    ULONG Length;
-    BOOLEAN Initialized;
-    HANDLE SsHandle;
-    LIST_ENTRY InLoadOrderModuleList;
-    LIST_ENTRY InMemoryOrderModuleList;
-    LIST_ENTRY InInitializationOrderModuleList;
-    PVOID      EntryInProgress;
-
-} PEB_LDR_DATA, * PPEB_LDR_DATA;
-
-//here we don't want to use any functions imported form extenal modules
-
-typedef struct _LDR_DATA_TABLE_ENTRY {
-    LIST_ENTRY  InLoadOrderModuleList;
-    LIST_ENTRY  InMemoryOrderModuleList;
-    LIST_ENTRY  InInitializationOrderModuleList;
-    void* BaseAddress;
-    void* EntryPoint;
-    ULONG   SizeOfImage;
-    UNICODE_STRING FullDllName;
-    UNICODE_STRING BaseDllName;
-    ULONG   Flags;
-    SHORT   LoadCount;
-    SHORT   TlsIndex;
-    HANDLE  SectionHandle;
-    ULONG   CheckSum;
-    ULONG   TimeDateStamp;
-} LDR_DATA_TABLE_ENTRY, * PLDR_DATA_TABLE_ENTRY;
-
-
-typedef struct _PEB {
-    BOOLEAN InheritedAddressSpace;
-    BOOLEAN ReadImageFileExecOptions;
-    BOOLEAN BeingDebugged;
-    BOOLEAN SpareBool;
-    HANDLE Mutant;
-
-    PVOID ImageBaseAddress;
-    PPEB_LDR_DATA Ldr;
-    // ...
-
-} PEB, * PPEB;
-
-#endif //__NTDLL_H__
-
+// We can hash in compile-time to avoid using string comparing in the shellcode. That saves time & space
 template <typename T, T value>
 INLINE constexpr T ensure_constexpr() { return value; }
 #define CONSTEXPR(x) ensure_constexpr<decltype(x), x>()
@@ -118,100 +55,163 @@ INLINE constexpr int adler32(const char* data) {
 
 #define HASH(x) CONSTEXPR(adler32(x))
 
-INLINE LPVOID get_proc_address(WCHAR* module_name) {
-    PPEB peb = NULL;
-    #if defined(_WIN64)
-        peb = reinterpret_cast<PPEB>(__readgsqword(0x60));
-    #else
-        peb = reinterpret_cast<PPEB>(__readfsdword(0x30));
+// On windows we use PEB & TEB
+#ifdef _WINDOWS 
+    #include <windows.h>
+
+    #ifndef __NTDLL_H__
+
+    #ifndef TO_LOWERCASE
+    #define TO_LOWERCASE(out, c1) (out = (c1 <= 'Z' && c1 >= 'A') ? c1 = (c1 - 'A') + 'a': c1)
     #endif
-    PPEB_LDR_DATA ldr = peb->Ldr;
-    LIST_ENTRY list = ldr->InLoadOrderModuleList;
 
-    PLDR_DATA_TABLE_ENTRY Flink = *((PLDR_DATA_TABLE_ENTRY*)(&list));
-    PLDR_DATA_TABLE_ENTRY curr_module = Flink;
+    typedef struct _UNICODE_STRING {
+        USHORT Length;
+        USHORT MaximumLength;
+        PWSTR  Buffer;
 
-    while (curr_module != NULL && curr_module->BaseAddress != NULL) {
-        if (curr_module->BaseDllName.Buffer == NULL) continue;
-        WCHAR* curr_name = curr_module->BaseDllName.Buffer;
+    } UNICODE_STRING, * PUNICODE_STRING;
 
-        size_t i = 0;
-        for (i = 0; module_name[i] != 0 && curr_name[i] != 0; i++) {
-            WCHAR c1, c2;
-            TO_LOWERCASE(c1, module_name[i]);
-            TO_LOWERCASE(c2, curr_name[i]);
-            if (c1 != c2) break;
+    typedef struct _PEB_LDR_DATA {
+        ULONG Length;
+        BOOLEAN Initialized;
+        HANDLE SsHandle;
+        LIST_ENTRY InLoadOrderModuleList;
+        LIST_ENTRY InMemoryOrderModuleList;
+        LIST_ENTRY InInitializationOrderModuleList;
+        PVOID      EntryInProgress;
+
+    } PEB_LDR_DATA, * PPEB_LDR_DATA;
+
+    //here we don't want to use any functions imported form extenal modules
+
+    typedef struct _LDR_DATA_TABLE_ENTRY {
+        LIST_ENTRY  InLoadOrderModuleList;
+        LIST_ENTRY  InMemoryOrderModuleList;
+        LIST_ENTRY  InInitializationOrderModuleList;
+        void* BaseAddress;
+        void* EntryPoint;
+        ULONG   SizeOfImage;
+        UNICODE_STRING FullDllName;
+        UNICODE_STRING BaseDllName;
+        ULONG   Flags;
+        SHORT   LoadCount;
+        SHORT   TlsIndex;
+        HANDLE  SectionHandle;
+        ULONG   CheckSum;
+        ULONG   TimeDateStamp;
+    } LDR_DATA_TABLE_ENTRY, * PLDR_DATA_TABLE_ENTRY;
+
+
+    typedef struct _PEB {
+        BOOLEAN InheritedAddressSpace;
+        BOOLEAN ReadImageFileExecOptions;
+        BOOLEAN BeingDebugged;
+        BOOLEAN SpareBool;
+        HANDLE Mutant;
+
+        PVOID ImageBaseAddress;
+        PPEB_LDR_DATA Ldr;
+        // ...
+
+    } PEB, * PPEB;
+
+    #endif //__NTDLL_H__
+
+    INLINE LPVOID get_proc_address(WCHAR* module_name) {
+        PPEB peb = NULL;
+        #if defined(_WIN64)
+            peb = reinterpret_cast<PPEB>(__readgsqword(0x60));
+        #else
+            peb = reinterpret_cast<PPEB>(__readfsdword(0x30));
+        #endif
+        PPEB_LDR_DATA ldr = peb->Ldr;
+        LIST_ENTRY list = ldr->InLoadOrderModuleList;
+
+        PLDR_DATA_TABLE_ENTRY Flink = *((PLDR_DATA_TABLE_ENTRY*)(&list));
+        PLDR_DATA_TABLE_ENTRY curr_module = Flink;
+
+        while (curr_module != NULL && curr_module->BaseAddress != NULL) {
+            if (curr_module->BaseDllName.Buffer == NULL) continue;
+            WCHAR* curr_name = curr_module->BaseDllName.Buffer;
+
+            size_t i = 0;
+            for (i = 0; module_name[i] != 0 && curr_name[i] != 0; i++) {
+                WCHAR c1, c2;
+                TO_LOWERCASE(c1, module_name[i]);
+                TO_LOWERCASE(c2, curr_name[i]);
+                if (c1 != c2) break;
+            }
+            if (module_name[i] == 0 && curr_name[i] == 0)
+                return curr_module->BaseAddress;
+            curr_module = (PLDR_DATA_TABLE_ENTRY)curr_module->InLoadOrderModuleList.Flink;
         }
-        if (module_name[i] == 0 && curr_name[i] == 0)
-            return curr_module->BaseAddress;
-        curr_module = (PLDR_DATA_TABLE_ENTRY)curr_module->InLoadOrderModuleList.Flink;
-    }
-    return NULL;
-}
-
-INLINE LPVOID get_module_handle(LPVOID module, int hash) {
-    IMAGE_DOS_HEADER* idh = (IMAGE_DOS_HEADER*)module;
-    if (idh->e_magic != IMAGE_DOS_SIGNATURE) return NULL;
-
-    IMAGE_NT_HEADERS* nt_headers = (IMAGE_NT_HEADERS*)((BYTE*)module + idh->e_lfanew);
-    IMAGE_DATA_DIRECTORY* exportsDir = &(nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT]);
-    if (exportsDir->VirtualAddress == NULL) return NULL;
-
-    IMAGE_EXPORT_DIRECTORY* exp = (IMAGE_EXPORT_DIRECTORY*)(exportsDir->VirtualAddress + (ULONG_PTR)module);
-
-    // Iterate through names
-    for (SIZE_T i = 0; i < exp->NumberOfNames; i++) {
-        DWORD* nameRVA = (DWORD*)((exp->AddressOfNames + (BYTE*)module) + i * sizeof(DWORD));
-        WORD* nameIndex = (WORD*)((exp->AddressOfNameOrdinals + (BYTE*)module) + i * sizeof(WORD));
-        DWORD* funcRVA = (DWORD*)((exp->AddressOfFunctions + (BYTE*)module) + (*nameIndex) * sizeof(DWORD));
-        LPSTR curr_name = (LPSTR)(*nameRVA + (BYTE*)module);
-
-        if (adler32(curr_name) == hash) return (BYTE*)module + (*funcRVA);
-    }
-    return NULL;
-}
-
-/* 
-int dump_pe_section(char* file, char* section, char* output) {
-    FILE* inputFile = fopen(file, "rb");
-    if (inputFile == NULL) {
-        printf("Unable to open input file.\n");
-        return 1;
+        return NULL;
     }
 
-    FILE* outputFile = fopen(output, "wb");
-    if (outputFile == NULL) {
-        printf("Unable to open output file.\n");
-        fclose(inputFile);
-        return 2;
+    INLINE LPVOID get_module_handle(LPVOID module, int hash) {
+        IMAGE_DOS_HEADER* idh = (IMAGE_DOS_HEADER*)module;
+        if (idh->e_magic != IMAGE_DOS_SIGNATURE) return NULL;
+
+        IMAGE_NT_HEADERS* nt_headers = (IMAGE_NT_HEADERS*)((BYTE*)module + idh->e_lfanew);
+        IMAGE_DATA_DIRECTORY* exportsDir = &(nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT]);
+        if (exportsDir->VirtualAddress == NULL) return NULL;
+
+        IMAGE_EXPORT_DIRECTORY* exp = (IMAGE_EXPORT_DIRECTORY*)(exportsDir->VirtualAddress + (ULONG_PTR)module);
+
+        // Iterate through names
+        for (SIZE_T i = 0; i < exp->NumberOfNames; i++) {
+            DWORD* nameRVA = (DWORD*)((exp->AddressOfNames + (BYTE*)module) + i * sizeof(DWORD));
+            WORD* nameIndex = (WORD*)((exp->AddressOfNameOrdinals + (BYTE*)module) + i * sizeof(WORD));
+            DWORD* funcRVA = (DWORD*)((exp->AddressOfFunctions + (BYTE*)module) + (*nameIndex) * sizeof(DWORD));
+            LPSTR curr_name = (LPSTR)(*nameRVA + (BYTE*)module);
+
+            if (adler32(curr_name) == hash) return (BYTE*)module + (*funcRVA);
+        }
+        return NULL;
     }
 
-    IMAGE_DOS_HEADER dosHeader;
-    fread(&dosHeader, sizeof(IMAGE_DOS_HEADER), 1, inputFile);
+    /* 
+    int dump_pe_section(char* file, char* section, char* output) {
+        FILE* inputFile = fopen(file, "rb");
+        if (inputFile == NULL) {
+            printf("Unable to open input file.\n");
+            return 1;
+        }
 
-    fseek(inputFile, dosHeader.e_lfanew, SEEK_SET);
-
-    IMAGE_NT_HEADERS ntHeader;
-    fread(&ntHeader, sizeof(IMAGE_NT_HEADERS), 1, inputFile);
-
-    IMAGE_SECTION_HEADER sectionHeader;
-    for (int i = 0; i < ntHeader.FileHeader.NumberOfSections; i++) {
-        fread(&sectionHeader, sizeof(IMAGE_SECTION_HEADER), 1, inputFile);
-        if (strncmp((char*)sectionHeader.Name, section, 8) == 0) {
-            char* buffer = (char*)malloc(sectionHeader.SizeOfRawData);
-            fseek(inputFile, sectionHeader.PointerToRawData, SEEK_SET);
-            fread(buffer, sectionHeader.SizeOfRawData, 1, inputFile);
-            fwrite(buffer, sectionHeader.SizeOfRawData, 1, outputFile);
-            free(buffer);
-            fclose(outputFile);
+        FILE* outputFile = fopen(output, "wb");
+        if (outputFile == NULL) {
+            printf("Unable to open output file.\n");
             fclose(inputFile);
-            return 0;
+            return 2;
         }
-    }
-    fclose(outputFile);
-    fclose(inputFile);
-    return 3;
-}*/
+
+        IMAGE_DOS_HEADER dosHeader;
+        fread(&dosHeader, sizeof(IMAGE_DOS_HEADER), 1, inputFile);
+
+        fseek(inputFile, dosHeader.e_lfanew, SEEK_SET);
+
+        IMAGE_NT_HEADERS ntHeader;
+        fread(&ntHeader, sizeof(IMAGE_NT_HEADERS), 1, inputFile);
+
+        IMAGE_SECTION_HEADER sectionHeader;
+        for (int i = 0; i < ntHeader.FileHeader.NumberOfSections; i++) {
+            fread(&sectionHeader, sizeof(IMAGE_SECTION_HEADER), 1, inputFile);
+            if (strncmp((char*)sectionHeader.Name, section, 8) == 0) {
+                char* buffer = (char*)malloc(sectionHeader.SizeOfRawData);
+                fseek(inputFile, sectionHeader.PointerToRawData, SEEK_SET);
+                fread(buffer, sectionHeader.SizeOfRawData, 1, inputFile);
+                fwrite(buffer, sectionHeader.SizeOfRawData, 1, outputFile);
+                free(buffer);
+                fclose(outputFile);
+                fclose(inputFile);
+                return 0;
+            }
+        }
+        fclose(outputFile);
+        fclose(inputFile);
+        return 3;
+    }*/
 
 #endif
 
